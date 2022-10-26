efaf64dd8208d6f71bf5306a1e0fc07c927a1baf
Slawomir Bokszczanin <bokszczaninslawomir@gmail.com>
2022-10-26 22:12:19 +0200

    Change function

--- Printer/js/main.js
+++ Printer/js/main.js
@@ -3,10 +3,15 @@ const input2 = document.querySelector('#input-2');
 const output1 = document.querySelector('#output-1');
 const output2 = document.querySelector('#output-2');
 
-input1.addEventListener('keyup', function () {
+const output = () =>{
     output1.value = input1.value;
-})
+}
+
+input1.addEventListener('keyup', output)
 
 input2.addEventListener('keyup', function () {
     output2.value = input2.value;
 })
+
+//function () {
+//    output1.value = input1.value;
